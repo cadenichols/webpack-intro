73fea92742fc0ce6d8885d07a04f7d5f5d940371
Cade Nichols <c@codinghouse.co>
2016-09-19 13:01:27 -0700

    test

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React, { createClass } from 'react';
+
+const App = createClass({
+  render() {
+    return (
+      <h1>Hello React!</h1>
+    )
+  }
+})
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const webpack = require('webpack');
+
+module.exports = {
+  entry: './src/index.js',
+  output: { path: './build', filename: 'bundle.js' },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
+  }
+}
